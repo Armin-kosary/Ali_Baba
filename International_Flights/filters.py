188f9fce53558e7d17dded209289f987df22c313
Armin Kosari <Armynkosari@gmail.com>
2024-08-02 15:18:51 +0430

    Update

--- International_Flights/filters.py
+++ International_Flights/filters.py
@@ -3,8 +3,8 @@ from .models import InternationalFlight
 
 
 class InternationalFlightsListFilter(filter.FilterSet):
-    min_price = filter.NumberFilter(field_name="price", lookup_expr="lte")
-    max_price = filter.NumberFilter(field_name="price", lookup_expr="gte")
+    min_price = filter.NumberFilter(field_name="price", lookup_expr="gte")
+    max_price = filter.NumberFilter(field_name="price", lookup_expr="lte")
     class Meta:
         model = InternationalFlight
         fields = ["starting_point", "destination", "ticket_type", "departure_date", "return_date", "passengers"]
